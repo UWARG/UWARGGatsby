0d14746c563f7a2606c3e0a7f563908f8fb42cb6
Sanjelo <ddgwilli@uwaterloo.ca>
2018-04-24 14:03:40 -0400

    Fixed links I hope.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,12 +25,12 @@ const Layout = ({ children, data }) => (
         { href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css', rel: 'stylesheet' },
         { href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,600', rel: 'stylesheet', type: 'text/css' },
         { href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.cs', rel: 'stylesheet' },
+        { href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', rel: 'stylesheet' },
       ]}
       script={[
-        { href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', rel: 'stylesheet' },
-        { src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js' },
-        { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js' },
-        { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js' },
+        { src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js', crossorigin: 'anonymous' },
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js', crossorigin: 'anonymous' },
+        { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js', crossorigin: 'anonymous' },
       ]}
     />
     <Navbar />
