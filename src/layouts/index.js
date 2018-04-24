625a513ed4d1354d14a0716b8fcf1c0c7858629d
Sanjelo <ddgwilli@uwaterloo.ca>
2018-04-24 14:27:41 -0400

    Added links back in.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -21,6 +21,17 @@ const Layout = ({ children, data }) => (
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
       ]}
+      link={[
+        { href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css', rel: 'stylesheet' },
+        { href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,600', rel: 'stylesheet', type: 'text/css' },
+        { href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.cs', rel: 'stylesheet' },
+        { href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', rel: 'stylesheet' },
+      ]}
+      script={[
+        { src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js' },
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js' },
+        { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js' },
+      ]}
     />
     <Navbar />
     {children()}
