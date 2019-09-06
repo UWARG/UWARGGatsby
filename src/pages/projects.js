4628b000bc027e7246c03ac5668f9e96ad85e890
avlepage <kathara.100@gmail.com>
2019-09-05 20:42:08 -0400

    Updates to current projects and deliverables

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,13 +10,6 @@ const ProjectsPage = () => (
     </div>
     <div className="row">
       <div className="col-md-4 col-12 projectCard">
-        <Link className="noUnderline" style={{color: "#262626"}} to="/boreas">
-          <img className="img-fluid" src="boreas.jpg" />
-          <h4 style={{marginBottom: "0em", marginTop: ".2em"}}>Project Boreas</h4>
-          <small className="text-muted">The newest addition to the WARG fleet.</small>
-        </Link>
-      </div>
-      <div className="col-md-4 col-12 projectCard">
         <Link className="noUnderline" style={{color: "#262626"}} to="/hex">
           <img className="img-fluid" src="hex.png" />
           <h4 style={{marginBottom: "0em", marginTop: ".2em"}}>Project Hex</h4>
@@ -30,8 +23,6 @@ const ProjectsPage = () => (
           <small className="text-muted">The board behind it all. Custom designed and built by us, for us.</small>
         </Link>
       </div>
-    </div>
-    <div className="row">
       <div className="col-md-4 col-12 projectCard">
         <Link className="noUnderline" style={{color: "#262626"}} to="/trackingantenna">
           <img className="img-fluid" src="trackingantenna.png" />
@@ -39,6 +30,13 @@ const ProjectsPage = () => (
           <small className="text-muted">The tracking antenna enhances the range of WARG’s UAVs.</small>
         </Link>
       </div>
+    </div>
+    <div style={{marginTop: "3em"}} className="row">
+      <div className="col-12">
+        <h1>Future Goals</h1>
+      </div>
+    </div>
+    <div style={{marginBottom: "2em"}} className="row">
       <div className="col-md-4 col-12 projectCard">
         <Link className="noUnderline" style={{color: "#262626"}} to="/computervisionsystem">
           <img className="img-fluid" src="computervisionsystem.png" />
@@ -72,6 +70,15 @@ const ProjectsPage = () => (
           <img className="img-fluid" src="groundstationicon.png" />
           <h4 style={{marginBottom: "0em", marginTop: ".2em"}}>Ground Station</h4>
           <small className="text-muted">Seamless wireless control of all our systems.</small>
+        </Link>
+      </div>
+    </div>
+    <div style={{marginBottom: "2em"}} className="row">
+      <div className="col-md-4 col-12 projectCard">
+        <Link className="noUnderline" style={{color: "#262626"}} to="/boreas">
+          <img className="img-fluid" src="boreas.jpg" />
+          <h4 style={{marginBottom: "0em", marginTop: ".2em"}}>Project Boreas</h4>
+          <small className="text-muted">The newest addition to the WARG fleet.</small>
         </Link>
       </div>
     </div>
